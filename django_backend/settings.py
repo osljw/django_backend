22f24f3ed9bab0733fd1adfd4945ac523bab0ca4
ljw <ljw@ljw.com>
2023-06-02 15:32:29 +0800

    fix static

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -192,11 +192,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# python manage.py collectstatic 收集位置
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 # react static
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build', 'static'),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
