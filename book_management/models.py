7212304bf26473dce9b9dcdef16e9556f97bc492
123 <123@test.test>
2024-11-22 17:38:55 +0800

    update borrow

--- book_management/models.py
+++ book_management/models.py
@@ -4,13 +4,15 @@ from user_auth.models import User
 # Create your models here.
 
 class Book(models.Model):
+    uid = models.CharField(max_length = 13, unique = False, null = True, blank = True)
+    isbn = models.CharField(max_length = 13, unique = False, null = True, blank = True)
     title = models.CharField(max_length = 255)
-    author = models.CharField(max_length = 255)
-    isbn = models.CharField(max_length = 13, unique = False)
-    volume = models.CharField(max_length = 10, blank = True, null = True)  # 用于区分上下册等情况
+    author = models.CharField(max_length = 255, null = True, blank = True)
     publication_date = models.DateField(null = True, blank = True)
     publisher = models.CharField(max_length = 255, null = True, blank = True)
-    quantity = models.IntegerField(default = 1)
+    total_count = models.IntegerField(default = 1)
+    borrowed_count = models.IntegerField(default=0)
+    purchase_year = models.IntegerField(null=True, blank=True)
     # status = models.CharField(max_length=10, choices=[
     #     ('available', '可用'),
     #     ('borrowed', '已借出'),
